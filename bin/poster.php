a6a855e7b9ab3b640ff78c027b73d422e36103d1
masuhajime <masuhajime@gmail.com>
2013-02-18 10:59:37 +0900

    add battle command to poster

--- bin/poster.php
+++ bin/poster.php
@@ -16,6 +16,36 @@ $params = array(
     'commandID' => 30,
     'power' => 3,
 );
+$url = 'http://dragonx.asobism.co.jp/top/beauty/beautySave.php?HTTP_UTIL=1';
+$params = array(
+"head" => 2305012,
+"body" => 2405013,
+"weapon" => 1300432,
+"hair" => 3100118,
+"face" => 3200012,
+"bg" => 3300023,
+"rare" => 2,
+"hflag" => 0,
+"job" => 15,
+);
+$url = 'http://dragonx.asobism.co.jp/top/equip/equipRequestChange.php?HTTP_UTIL=1';
+$params = array(
+'eid' => 11,
+'aid' => 104137785
+);
+$url = 'http://dragonx.asobism.co.jp/top/battle/checkBattle.php?HTTP_UTIL=1';
+$params = array(
+"userID" => "",
+"timeID" => "201302171200"
+);
+$url = 'http://dragonx.asobism.co.jp/top/battle/battleCommandProcess.php?HTTP_UTIL=1';
+$params = array(
+"commandID" => intval($argv[1]),
+"power" => "3"
+);
+
+echo "url:".$url.PHP_EOL;
+var_dump($params);
 
 $client = new Client(app\helper\DlxUrl::URL_DRAGONX);
 $request = $client->post($url);
