2eb1dab8dd9cc3f348ed2755fc91c1cca44b80c5
masuhajime <masuhajime@gmail.com>
2013-02-24 15:23:15 +0900

    add data dir

--- conf/define.php
+++ conf/define.php
@@ -8,6 +8,6 @@ define('DIR_APP', DIR_BASE.'/app');
 define('DIR_HELPER', DIR_BASE.'/app/helper');
 define('DIR_MODEL', DIR_BASE.'/app/model');
 define('DIR_LIB', DIR_BASE.'/lib');
-define('DIR_WIKI', DIR_BASE.'/wiki');
+define('DIR_DATA', DIR_BASE.'/data');
 
 define('DIR_VENDOR', DIR_BASE.'/vendor');
